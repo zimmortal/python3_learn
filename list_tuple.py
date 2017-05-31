589db18a8efa48665678c279f9bfa3899c884697
zimmortal <897201893@qq.com>
2017-05-31 10:19:36 +0800

    Update list_tuple.py

--- list_tuple.py
+++ list_tuple.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 import sys
 classmates = ['a','v','c','s','w','1',2]
-print(classmates)
-classmates.pop()
-print(classmates)
-print(classmates.pop(2))
-print(classmates)
-print(len(classmates))
-print(classmates.insert(2,';'))
-print(classmates)
+print(classmates)  # ['a', 'v', 'c', 's', 'w', '1', 2]
+classmates.pop() 
+print(classmates)  # ['a', 'v', 'c', 's', 'w', '1']
+print(classmates.pop(2))  # c
+print(classmates)  # ['a', 'v', 's', 'w', '1']
+print(len(classmates)) # 5
+print(classmates.insert(2,';'))  #  insert的 返回值  为None
+
+print(classmates) # ['a', 'v', ';', 's', 'w', '1']
 
 L = [
         ['Apple', 'Google', 'Microsoft'],
         ['Java', 'Python', 'Ruby', 'PHP'],
         ['Adam', 'Bart', 'Lisa']
 ]
-print(L[0][0])
-print(L[1][1])
-print(L[2][2])
+print(L[0][0]) # Apple
+print(L[1][1]) # Python
+print(L[2][2]) # Lisa
 
 t = ('a','b','c','d',['A','B'])
-print(t)
+print(t) # ('a', 'b', 'c', 'd', ['A', 'B'])
 t[len(t)-1][0] = 'Z'
-print(t)
+print(t) # ('a', 'b', 'c', 'd', ['Z', 'B'])
